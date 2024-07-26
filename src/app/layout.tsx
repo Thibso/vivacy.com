3d1c0e0bf488fefa11305be5a328cc1920a796d2
Thibso <thibautsoulet33@gmail.com>
2024-07-26 09:16:13 +0200

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const mona = localFont({
+  src: [
+    {
+      path: "../../public/fonts/MonaSans-Light.woff2",
+      weight: "300",
+    },
+  ],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${mona.variable} font-mona`}>
+      <body>{children}</body>
     </html>
   );
 }
